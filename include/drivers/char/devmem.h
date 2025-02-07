14df0e8a53fa3c292d693a08201930f67db0d8aa
Jar387 <aiyu2009@126.com>
2025-02-07 20:00:54 +0800

    mem util chardev

--- include/drivers/char/devmem.h
+++ include/drivers/char/devmem.h
@@ -1,6 +1,20 @@
 #ifndef DEVMEM_H
 #define DEVMEM_H
 
+#define MINOR_DEVMEM_MEM 1
+#define MINOR_DEVMEM_NULL 2
+#define MINOR_DEVMEM_PORT 3
+#define MINOR_DEVMEM_FULL 4
+#define MINOR_DEVMEM_ZERO 5
+#define MINOR_DEVMEM_RANDOM 6
+
+#define MEM_DEV_HI (char*)0xc0100000
+#define MEM_DEV_LO (char*)0xc0000000
+
+#define PORT_DEV_HI 0xffff
+#define PORT_DEV_LO 0
+
+
 void init_devmem();
 
 #endif
