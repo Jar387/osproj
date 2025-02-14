cbf94163c98a154614d1f8288f3b6d60328153e3
Jar387 <aiyu2009@126.com>
2025-02-14 18:33:52 +0800

    20250214 final

--- drivers/char/serial.c
+++ drivers/char/serial.c
@@ -4,6 +4,7 @@
 #include <kernel.h>
 #include <errno.h>
 #include <stddef.h>
+#include <apic.h>
 
 static unsigned short ports[4];
 
@@ -15,14 +16,14 @@ init_one_com(unsigned short port, int index)
 	outb(SERIAL_DATA(port), 0x03);
 	outb(SERIAL_INT(port), 0x00);
 	outb(SERIAL_LINECTL(port), 0x03);
-	outb(SERIAL_FIFO(port), 0xC7);
+	outb(SERIAL_FIFO(port), 0);
 	outb(SERIAL_MODEM(port), 0x0B);
 	outb(SERIAL_MODEM(port), 0x1E);
 	outb(SERIAL_DATA(port), 0xAE);
 	if (inb(SERIAL_DATA(port)) != 0xAE) {
 		return 1;
 	}
-	outb(SERIAL_MODEM(port), 0x0F);
+	outb(SERIAL_MODEM(port), 0x0B);
 	ports[index] = port;
 	return 0;
 }
@@ -42,5 +43,8 @@ serial_init()
 {
 	for (int i = 0; (i < 4) && ((BIOS_DATA->serial_addr[i]) != 0); i++) {
 		init_one_com(BIOS_DATA->serial_addr[i], i);
+	}
+	for(int i=0;i<24;i++){
+		ioapic_enable(i%14, i);
 	}
 }
